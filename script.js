3202d7254010b03bc91cce1c6ca85d75f27f86c7
BigBobandy <jdykes207@gmail.com>
2023-03-28 21:37:31 -0400

    updated css and script.js
    brought in my html elements into script.js and styled a few things in css

---
+++ script.js
@@ -0,0 +1,5 @@
+const todoListElement = document.getElementById("list-of-todos");
+const inputElement = document.getElementById("user-input");
+const submitButton = document.getElementById("submit-button");
+const clearButton = document.getElementById("clear-button");
+const todoCounter = document.getElementById("todo-number");
